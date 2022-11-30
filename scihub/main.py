fb46425199374c771c277d06abf4881b54741176
Rhythmicc <RhythmLian@outlook.com>
2022-11-30 19:08:01 +0800

    update

--- scihub/main.py
+++ scihub/main.py
@@ -5,31 +5,60 @@ from . import *
 app = Commander(name)
 
 
-@app.command()
-def dl(meeting: str = ""):
+def pageCheck(url, driver, status):
     """
-    下载论文
-
-    :param meeting: 会议
+    解析论文信息
     """
-    import re
-
-    keywords = ask(
-        {
-            "type": "input",
-            "message": "请输入关键词",
-        }
-    )
-
     from selenium.webdriver.common.by import By
 
-    status = QproDefaultConsole.status(
-        "正在打开浏览器..." if user_lang == "zh" else "Opening browser..."
-    )
+    status.update("正在解析论文信息")
+    driver.get(url)
 
-    status.start()
+    status.update("判断页面类型")
 
-    driver = getDriver()
+    try:
+        is_acm_paper = (
+            driver.find_element(By.TAG_NAME, "a").get_attribute("title")
+            == "ACM Digital Library home"
+        )
+    except:
+        is_acm_paper = False
+
+    if is_acm_paper:
+        status.update("正在解析ACM论文信息")
+        title = driver.find_element(By.CLASS_NAME, "citation__title").text.replace(
+            ": ", "："
+        )
+        meeting = driver.find_element(
+            By.CLASS_NAME, "epub-section__title"
+        ).text.split()[0]
+        year = (
+            driver.find_element(By.CLASS_NAME, "CitationCoverDate")
+            .text.strip()
+            .split()[-1]
+        )
+    else:
+        status.update("正在解析IEEE论文信息")
+        title = driver.find_element(
+            By.XPATH,
+            '//*[@id="LayoutWrapper"]/div/div/div/div[3]/div/xpl-root/div/xpl-document-details/div/div[1]/section[2]/div/xpl-document-header/section/div[2]/div/div/div[1]/div/div[1]/h1/span',
+        ).text
+        _info = (
+            driver.find_element(By.CLASS_NAME, "stats-document-abstract-publishedIn")
+            .find_element(By.TAG_NAME, "a")
+            .text.strip()
+        )
+        meeting = _info.split()[-1][1:-1]
+        year = _info.split()[0]
+
+    return meeting, year, title
+
+
+def getUrl(url, driver, status):
+    """
+    获取论文链接
+    """
+    from selenium.webdriver.common.by import By
 
     status.update("正在打开Sci-Hub..." if user_lang == "zh" else "Opening Sci-Hub...")
 
@@ -37,7 +66,7 @@ def dl(meeting: str = ""):
 
     status.update("正在搜索..." if user_lang == "zh" else "Searching...")
 
-    driver.find_element(By.ID, "request").send_keys(keywords)
+    driver.find_element(By.ID, "request").send_keys(url)
     driver.find_element(By.TAG_NAME, "button").click()
 
     status.update("正在解析..." if user_lang == "zh" else "Parsing...")
@@ -48,62 +77,54 @@ def dl(meeting: str = ""):
                 QproWarnString,
                 "未找到相关论文" if user_lang == "zh" else "No related papers found",
             )
-            status.update("正在关闭浏览器..." if user_lang == "zh" else "Closing browser...")
-            closeDriver()
-            status.stop()
-            return
+            return None
     except:
         pass
 
     try:
-        content = driver.find_element(By.ID, "citation")
-        i = content.find_element(By.TAG_NAME, "i").text
         # get url from button
         url = driver.find_element(By.TAG_NAME, "button").get_attribute("onclick")
         url = url.split("'")[1][:-1]
-
-        # get paper title
-        content = content.text
     except:
         QproDefaultConsole.print(
             QproWarnString,
             "未找到相关论文" if user_lang == "zh" else "No related papers found",
         )
-        status.update("正在关闭浏览器..." if user_lang == "zh" else "Closing browser...")
-        closeDriver()
-        status.stop()
-        return
+        return None
+
+
+@app.command()
+def dl(url: str = ""):
+    """
+    下载ACM/IEEE论文
+
+    :param url: 论文链接
+    """
+    import re
+
+    url = ask(
+        {
+            "type": "input",
+            "message": "请输入论文链接",
+        }
+    )
+
+    status = QproDefaultConsole.status(
+        "正在打开浏览器..." if user_lang == "zh" else "Opening browser..."
+    )
+
+    status.start()
+    driver = getDriver()
+
+    meeting, year, title = pageCheck(url, driver, status)
+    url = getUrl(url, driver, status)
 
     status.update("正在关闭浏览器..." if user_lang == "zh" else "Closing browser...")
     closeDriver()
+    if not url:
+        status.stop()
+        return
     status.update("正在解析..." if user_lang == "zh" else "Parsing...")
-
-    title = i.split(".")[0].strip()
-
-    # get meeting
-    if meeting == "":
-        meeting = content.split(".")[1].strip().split()[-1][1:-1]
-        if meeting not in ["HPCA", "ASPLOS", "ISCA", "MICRO", "EMC2"]:
-            status.stop()
-            QproDefaultConsole.print(
-                QproWarnString,
-                f"未识别: {meeting}, 请修正!"
-                if user_lang == "zh"
-                else f"Unrecognized: {meeting}, please fix!",
-            )
-
-            from . import _ask
-
-            meeting = _ask(
-                {
-                    "type": "input",
-                    "message": "请输入会议简称"
-                    if user_lang == "zh"
-                    else "Please enter the conference abbreviation",
-                }
-            )
-            status.start()
-    year = re.findall(r"\d{4}", content)[0]
 
     # move file to meeting dir
     work_path = config.select("work_path")
@@ -115,7 +136,6 @@ def dl(meeting: str = ""):
     status.update("正在下载..." if user_lang == "zh" else "Downloading...")
 
     # download
-
     path = os.path.join(
         config.select("work_path"),
         meeting,
